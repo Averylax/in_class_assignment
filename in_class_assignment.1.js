3c5693d1f04199609b95689f05cdfd478ec97bb8
avery.white <averydouglascwhite@usf.edu>
2025-01-30 16:25:23 -0500

    Task 3

--- in_class_assignment.1.js
+++ in_class_assignment.1.js
@@ -1,4 +1,4 @@
-// Task 1
+// Task 2
 let scores = 70; 
 if (scores >= 60) {
     console.log(`Passed`);
@@ -6,3 +6,8 @@ if (scores >= 60) {
     console.log(`Failed`);
 };
 
+// Task 3
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);  
+}
