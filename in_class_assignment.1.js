2acadc355d9bd364df949898e9db56d3129db6a8
avery.white <averydouglascwhite@usf.edu>
2025-01-30 16:36:42 -0500

    Task 4

--- in_class_assignment.1.js
+++ in_class_assignment.1.js
@@ -7,7 +7,19 @@ if (scores >= 60) {
 };
 
 // Task 3
-
 for (let i = 10; i >= 1; i--) {
     console.log(i);  
 }
+
+// Task 3
+let num = 2;
+while (num <= 10) {
+    console.log(num);
+    num += 2;
+}
+
+// Task 4
+function jeff(num) {
+    return num * num;
+}
+console.log(jeff(4));
